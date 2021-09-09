599bcd31ec76372a9257b4c305a1eeb813800f8e
Jared Montalbo <33338660+jaredmontalbo@users.noreply.github.com>
2021-09-09 01:33:58 -0400

    Added the completed Exercise 1

---
+++ Exercise1/src/main/java/com/mycompany/exercise1/ProjectGutenberg.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.exercise1;
+
+/**
+ *
+ * @author Jared
+ */
+public class ProjectGutenberg {
+    public static void main(String[] args) {
+        System.out.print("Did you know that in 1440 (or thereabouts), Johannes Gutenberg invented the printing press?");
+        System.out.print("He started out as a goldsmith!");
+        System.out.print("His invention made it easy to print and distribute books to anyone who wanted one.");
+        System.out.print("We are like a modern Gutenberg, printing whatever we want to the console with ease.");
+    }
+}
